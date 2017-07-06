72611be605b31d9a65f91e7ea4f51dd4b744e9b1
eacrosby <eacrosby@rsglab.com>
2017-07-06 15:02:06 -0400

    testing somethings to get subtitles to highlight with video

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
-const videoPlayer = document.querySelector('video');
-const subtitles = document.querySelector('captionArea');
+const subtitles = document.getElementById('captionArea').getElementsByTagName('span');
+let now = video.currentTime;
+
+video.addEventListener('timeupdate', function (){
+    for (const i = 0, l = subtitles.length; i < l, i++) {
+        if (now >= subtitles[i].getAttribute('data-start') &&
+           now <= subtitles[i].getAttribute('data-end')) {
+            subtitles[i].className = "highlight";
+        } else {
+            subtitles[i].className = "";
+        }
+    }
+})
